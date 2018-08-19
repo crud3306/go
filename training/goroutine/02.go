6f5515fb75386de0b752e0b89f0803e4aa832a8a
qianming <crud3306@163.com>
2018-08-20 06:18:57 +0800

    test

--- training/goroutine/02.go
+++ training/goroutine/02.go
@@ -10,8 +10,8 @@ var ch chan int = make(chan int)
 func foo() {
 	for j:=0; j<10; j++ {
 		fmt.Println("iam foo")
-	    cc := <- ch // 从ch取数据，如果ch中还没放数据，那就挂起main线，直到foo函数中放数据为止
-	    fmt.Println(cc)
+		cc := <- ch // 从ch取数据，如果ch中还没放数据，那就挂起main线，直到foo函数中放数据为止
+		fmt.Println(cc)
 	}
 }
 
@@ -19,9 +19,7 @@ func main() {
 
 	fmt.Println(111)    
 
-    
-
-    fmt.Println(222)    
+	fmt.Println(222)    
 
     ch <- 0 // 向ch中加数据，如果没有其他goroutine来取走这个数据，那么挂起foo, 直到main函数把0这个数据拿走
     go foo()
